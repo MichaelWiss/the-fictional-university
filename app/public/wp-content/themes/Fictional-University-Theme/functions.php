38796fbf8e846a7fc6dd2510942ade27b59f6a54
MichaelWiss <michael.wiss@gmail.com>
2023-10-14 12:38:39 -0500

    css bug

--- app/public/wp-content/themes/Fictional-University-Theme/functions.php
+++ app/public/wp-content/themes/Fictional-University-Theme/functions.php
@@ -1,8 +1,7 @@
 <?php
 
 function university_files() {
-    echo "Does this output to the actual page?";
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_enqueue_style( 'university_main_styles', get_stylesheet_uri() );
 }
 
-add_action('wp_enqueue_scripts', 'university_files');
+add_action( 'wp_enqueue_scripts', 'university_files' );
