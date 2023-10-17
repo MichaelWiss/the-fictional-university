d4f7a8e95dad3599be1d8f01816724d0a658bf19
MichaelWiss <michael.wiss@gmail.com>
2023-10-17 17:28:11 -0500

    links

--- app/public/wp-content/themes/The_Fictional_University/page.php
+++ app/public/wp-content/themes/The_Fictional_University/page.php
@@ -4,12 +4,14 @@
 
   while(have_posts()) {
     the_post(); ?>
-    <<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+   <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(
+        <?php echo get_theme_file_uri('/images/ocean.jpg') ?>
+      )"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p>DO NOT FORGET TO CHANGE ME LATER</p>
         </div>
       </div>
     </div>
@@ -20,14 +22,15 @@
           <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
         </p>
       </div>
-
+<!--
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div>
+      </div
+  -->
 
       <div class="generic-content">
         <?php the_content(); ?>
