aa3d6780642813c3a03b02f6496569dda8e7eaea
MichaelWiss <michael.wiss@gmail.com>
2023-10-24 16:00:08 -0500

    side menu

--- app/public/wp-content/themes/The_Fictional_University/page.php
+++ app/public/wp-content/themes/The_Fictional_University/page.php
@@ -31,9 +31,13 @@
           ?>
 
       
-
+      <?php 
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+      if ($theParent or $testArray) { ?>      
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
           <?php 
             if ($theParent) {
@@ -48,7 +52,8 @@
             ));
            ?>
         </ul>
-            </div>
+      </div>
+      <?php } ?>
 
       <div class="generic-content">
         <?php the_content(); ?>
