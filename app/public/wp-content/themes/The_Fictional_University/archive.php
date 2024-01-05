bf28571a049a1b0833aeb332f39542919203e3da
MichaelWiss <michael.wiss@gmail.com>
2024-01-05 17:17:26 -0600

    archive function

--- app/public/wp-content/themes/The_Fictional_University/archive.php
+++ app/public/wp-content/themes/The_Fictional_University/archive.php
@@ -7,12 +7,7 @@ get_header(); ?>
         <?php echo get_theme_file_uri('/images/ocean.jpg') ?>
       )"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php if (is_category()) {
-            single_cat_title();
-        }
-        if (is_author()) {
-            echo 'Posts by '; the_author();
-        } ?></h1>
+        <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
         <div class="page-banner__intro">
           <p>Keep up with our latest news</p>
         </div>
