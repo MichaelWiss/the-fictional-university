b05b0916c6c8c3fc1dfb79dc230ac3aa3ee1ba93
MichaelWiss <michael.wiss@gmail.com>
2024-01-10 17:29:07 -0600

    new post type

--- app/public/wp-content/themes/The_Fictional_University/functions.php
+++ app/public/wp-content/themes/The_Fictional_University/functions.php
@@ -17,3 +17,14 @@ function university_features() {
   add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'university_features');
+
+function university_post_types() {
+  register_post_type('event', array(
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events'
+    )
+  ));
+}
+
+add_action('init', 'university_post_types');
