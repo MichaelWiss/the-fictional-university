56a0df2d1db6778a89176be5bec3f6c4e1928062
MichaelWiss <michael.wiss@gmail.com>
2024-01-23 18:58:38 -0600

    related programs links

--- app/public/wp-content/themes/The_Fictional_University/single-event.php
+++ app/public/wp-content/themes/The_Fictional_University/single-event.php
@@ -27,9 +27,11 @@
         <?php
           $relatedPrograms = get_field('related_programs');
 
-          foreach($relatedPrograms as $program) {
-            echo get_the_title($program);
-          }
+          foreach($relatedPrograms as $program) { ?>
+
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+            
+         <?php }
           
          ?>
     </div>
